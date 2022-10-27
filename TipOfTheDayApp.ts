c123dd5020298418e8f4dcd3f18ef205a956880b
Kevin Aleman <kaleman960@gmail.com>
2022-10-27 14:25:40 -0700

    Support for images & multiple messages

--- TipOfTheDayApp.ts
+++ TipOfTheDayApp.ts
@@ -36,40 +36,42 @@ export class TipOfTheDayApp extends App implements IPostUserCreated, IPostUserLo
     }
 
     public async executePostUserLoggedIn(user: IUser, read: IRead, http: IHttp, persistence: IPersistence, modify: IModify): Promise<void> {
-        const [loginInfo] = await read.getPersistenceReader().readByAssociations([
+        const [loginInfo = { loginCount: 1, lastLogin: new Date() }] = await read.getPersistenceReader().readByAssociations([
             new RocketChatAssociationRecord(RocketChatAssociationModel.USER, user.id),
             new RocketChatAssociationRecord(RocketChatAssociationModel.MISC, 'loginInfo'),
         ]) as Array<{ lastLogin: Date, loginCount: number }>;
 
-        // If the user has never logged in before, we don't want to send them a message
-        if (!loginInfo) {
-            await persistence.createWithAssociations({
-                loginCount: 1,
-                lastLogin: new Date(),
-            }, [
-                new RocketChatAssociationRecord(RocketChatAssociationModel.USER, user.id),
-                new RocketChatAssociationRecord(RocketChatAssociationModel.MISC, 'loginInfo'),
-            ]);
+    /*  // If the user has never logged in before, we don't want to send them a message
+        // TODO: This is a hack, we should be able to check if the user is new
+        if (loginInfo) {
+            const today = new Date();
+            today.setHours(0,0,0,0);
+    
+            if (loginInfo.lastLogin > today) {
+                this.getLogger().log('User has already logged in today');
+                return;
+            }
+        } */
 
-            return;
-        }
-
-        const today = new Date();
-        today.setHours(0, 0, 0, 0);
-
-        if (loginInfo.lastLogin > today) {
-            return;
-        }
+        await persistence.createWithAssociations({
+            loginCount: 1,
+            lastLogin: new Date(),
+        }, [
+            new RocketChatAssociationRecord(RocketChatAssociationModel.USER, user.id),
+            new RocketChatAssociationRecord(RocketChatAssociationModel.MISC, 'loginInfo'),
+        ]);
 
         const tip = getRandomTip();
         const blocks = modify.getCreator().getBlockBuilder();
 
+        // For future self: get user's language and translate accordingly
+        await sendDirectMessage(read, modify, user, tipOfTheDay.replace('%s', tip.message));
+
+        // Send a 2nd message to avoid size limitations of blocks
         if (tip.image) {
             blocks.addImageBlock({ imageUrl: tip.image, altText: 'Tip of the Day' });
+            await sendDirectMessage(read, modify, user, '', blocks);
         }
-
-        // For future self: get user's language and translate accordingly
-        await sendDirectMessage(read, modify, user, tipOfTheDay.replace('%s', tip.message), blocks);
 
         await persistence.updateByAssociations([
             new RocketChatAssociationRecord(RocketChatAssociationModel.USER, user.id),
